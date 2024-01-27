5ef93bc7dbf52344c00ede46907b6fd280fc8779
ricardojch.12@gmail.com <ricardojch.12@gmail.com>
2024-01-27 18:07:57 -0500

    Integracion de Baner Richi

---
+++ MECHANIC/src/components/banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+    return (
+    <div className="bg-[#185866] text-white py-4 text-center">
+            <h1 className="text-4xl font-bold">MECHANIC</h1>
+        <p className="text-lg">Tu solución para servicios automotrices</p>
+    </div>
+    );
+};
+
+export default Banner;
---
+++ MECHANIC/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+    <div className="container mx-auto mt-10 text-center">
+        <h2 className="text-4xl font-bold mb-4">Bienvenido a MECHANIC</h2>
+        <p className="text-lg mb-8">
+        Tu destino confiable para servicios automotrices.
+        </p>
+    </div>
+    );
+};
+
+export default Home;
